516f0aefea11a2bb5d8747330be5e79c8f661706
Margit Saal <gussaalma@student.gu.se>
2017-03-10 13:10:56 +0100

    New controller code

--- Arduino/Controls.ino
+++ Arduino/Controls.ino
@@ -2,55 +2,57 @@
 
 Odometer encoderLeft, encoderRight;
 Car car;
+SR04 frontSensor, backSensor;
 
-const float fSpeed = 0.5; //a ground speed (m/sec) for going forward
-const float bSpeed = -0.5; //a ground speed (m/sec)y for going backward
-const int lDegrees = -75; //degrees to turn left
-const int rDegrees = 75; //degrees to turn right
+const int TRIGGER1_PIN = 6; //D6
+const int ECHO1_PIN = 5; //D5
+const int TRIGGER_PIN = 4;
+const int ECHO_PIN = 7;
 const int encoderLeftPin = 2;
 const int encoderRightPin = 3;
 
+
 void setup() {
   Serial3.begin(9600);
+  Serial.begin(19200);
   encoderLeft.attach(encoderLeftPin);
   encoderRight.attach(encoderRightPin);
+  frontSensor.attach(TRIGGER_PIN, ECHO_PIN);
+  backSensor.attach(TRIGGER1_PIN, ECHO1_PIN);
   encoderLeft.begin();
   encoderRight.begin();
   car.begin(encoderLeft, encoderRight); //initialize the car using the encoders
-  car.enableCruiseControl(); //using default PID values
 }
 
-void loop() {
-  car.updateMotors();
-  handleInput();
-}
 
-void handleInput() { //handle serial input if there is any
-  if (Serial3.available()) {
+void loop() {  
+   if (frontSensor.getMedianDistance() != 0 && frontSensor.getMedianDistance() < 15) {
+    car.setMotorSpeed(0,0);
+  } 
+
+   if(backSensor.getMedianDistance() !=0 && backSensor.getMedianDistance() <15) {
+    car.setMotorSpeed(0,0);
+   }
+   
+  if(Serial3.available()){
     char input = Serial3.read(); //read everything that has been received so far and log down the last entry
-    switch (input) {
-      case 'l': //rotate counter-clockwise going forward
-        car.setSpeed(fSpeed);
-        car.setAngle(lDegrees);
-        break;
-      case 'r': //turn clock-wise
-        car.setSpeed(fSpeed);
-        car.setAngle(rDegrees);
-        break;
-      case 'f': //go ahead
-        car.setSpeed(fSpeed);
-        car.setAngle(0);
-        break;
-      case 'b': //go back
-        car.setSpeed(bSpeed);
-        car.setAngle(0);
-        break;
-        case 's':
-        car.setSpeed(0);
-        car.setAngle(0);
-      default: //if you receive something that you don't know, just stop
-        car.setSpeed(0);
-        car.setAngle(0);
+       Serial.println(input);
+      if(input == 'f'){
+        car.setMotorSpeed(50,50);
+      } 
+      if(input == 'b'){
+        car.setMotorSpeed(-50,-50);
+      }
+      if(input == 's'){
+        car.setMotorSpeed(0,0);
     }
-  }
-}
+      if(input == 'l'){
+        car.setMotorSpeed(0,50);
+      }
+      if(input == 'r'){
+        car.setMotorSpeed(50,0);
+      } 
+    }
+    }
+
+
