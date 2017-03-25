3e5db5870a7b7dc1855ba74bc68e9ad26d51bf4c
Qing Lin <mitaosi@gmail.com>
2017-03-25 21:06:47 +0100

    Upgrade Obstacle check fucntion and fix bug
    Upgrade Obstacle check fucntion and fix a few bugs;
    Add gyroscope support

--- Arduino/Controls/Controls.ino
+++ Arduino/Controls/Controls.ino
@@ -1,11 +1,17 @@
 #include <Smartcar.h>
+
+#include <Smartcar.h>
+
+#include <Smartcar.h>
 #include <Wire.h>
-#include <Servo.h>
+
 
 Odometer encoderLeft, encoderRight;
-SR04 frontSensor, backSensor;
+SR04 frontSensor;
+SR04 backSensor; 
+Gyroscope gyro;
 Car car;
-Servo carServo;
+
 
 const int TRIGGER1_PIN = 6; //D6
 const int ECHO1_PIN = 5; //D5
@@ -14,37 +20,57 @@ const int ECHO_PIN = 7;
 const int encoderLeftPin = 2;
 const int encoderRightPin = 3;
 
-const int fwSpeed =  50; //set forward speed
-const int bwSpeed = -50; //set backward speed
-const int rDegree =  50; //set degrees to turn right
-const int lDegree = -50; //set degrees to turn left
+const int fwSpeed =  70; //set forward speed
+const int bwSpeed = -70; //set backward speed
+const int rDegree =  75; //set degrees to turn right
+const int lDegree = -75; //set degrees to turn left
+char Direction = 'n';
+
+int frDistance;
+int baDistance;
 
 void setup() {
   Serial3.begin(9600);
   Serial.begin(19200);
+  
+  gyro.attach();
   encoderLeft.attach(encoderLeftPin);
   encoderRight.attach(encoderRightPin);
   frontSensor.attach(TRIGGER_PIN, ECHO_PIN);
   backSensor.attach(TRIGGER1_PIN, ECHO1_PIN);
+  
+  // start components
+  gyro.begin();
   encoderLeft.begin();
   encoderRight.begin();
-  car.begin(encoderLeft, encoderRight); //initialize the car using the encoders
+  car.begin(encoderLeft, encoderRight, gyro); //initialize the car using the encoders
+  
+  //ini Ultrasonic distance meansurement as 0
+  frDistance = 0;
+  baDistance = 0;
+  
 }
 
 
 void loop() {  
-   int frDistance = frontSensor.getDistance();
-   int baDistance = backSensor.getDistance();
+   frDistance = frontSensor.getDistance();
+   baDistance = backSensor.getDistance();
+   handleInput();
+   
+   int curSpeed = car.getSpeed();
+   if (curSpeed == 0){
+		Direction = 'n';
+   }
+   
+   if ( NoObstacle(frDistance) == false && Direction != 'b' && Direction != 'n'){
+    car.setSpeed(0);
+   }
+   if (NoObstacle(baDistance) == false && Direction == 'b') {
+    car.setSpeed(0);
+   }
+ 
 
-   car.updateMotors();
-   handleInput(); 
-   if (frDistance != 0 && frDistance < 15) {
-    car.stop();
-   }
-   if (baDistance != 0 && baDistance < 15) {
-    car.stop();
-   }
-   ultsensorConflict();
+   //ultsensorConflict();
    
 }
    
@@ -52,29 +78,54 @@ void loop() {
 void handleInput(){
   if(Serial3.available()){
     char input = Serial3.read(); //read everything that has been received so far and log down the last entry
-       Serial.println(input);
-      if(input == 'f'){
+    switch (input) {
+      case 'f': //forward
+		Direction = 'f';
         car.setSpeed(fwSpeed);
-      } 
-      if(input == 'b'){
+        car.setAngle(0);
+        break;
+      case 'b': //backward
+		Direction = 'b';
         car.setSpeed(bwSpeed);
-      }
-      if(input == 's'){
+        car.setAngle(0);
+        break;
+      case 'l': //turn left
+		Direction = 'l';
+        car.setSpeed(fwSpeed);
+        car.setAngle(lDegree);
+        break;
+      case 'r': //turn right
+		Direction = 'r';
+        car.setSpeed(fwSpeed);
+        car.setAngle(rDegree);
+        break;
+      case 's': //stop car
         car.setSpeed(0);
+        car.setAngle(0);
+        break;
+      default: //if there isn't any command
+        car.setSpeed(0);
+        car.setAngle(0);
     }
-      if(input == 'l'){
-        car.setSpeed(fwSpeed);
-        car.setAngle(lDegrees);
-      }
-      if(input == 'r'){
-        car.setSpeed(fwSpeed);
-        car.setAngle(rDegrees);
-      } 
   }
 }
-	
-// incase both front ultrasonicSensor and back ultrasonicSensor are values at same time.	
-ultsensorConflict(){
+
+
+boolean NoObstacle(int distance){
+	if (distance > 20){
+		return true;
+	}
+	if (distance == 0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+  
+// incase both front ultrasonicSensor and back ultrasonicSensor are values at same time.  
+/*(void ultsensorConflict(){
  if((frontSensor.getDistance()&&backSensor.getDistance())<15){
    car.stop();
    Serial.write("1");
@@ -84,5 +135,4 @@ ultsensorConflict(){
   Serial.write("0");
    car.setSpeed(50);
  }
-}
-
+}*/
