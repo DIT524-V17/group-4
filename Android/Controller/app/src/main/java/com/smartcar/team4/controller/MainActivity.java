697efafdfbcb5366190e481ea1d05f156a9f4123
Andrius Sakalas <a.sakalas2@gmail.com>
2017-04-04 11:37:02 +0200

    Added Joystick to controls with Margit Saal

--- Android/Controller/app/src/main/java/com/smartcar/team4/controller/MainActivity.java
+++ Android/Controller/app/src/main/java/com/smartcar/team4/controller/MainActivity.java
@@ -2,13 +2,9 @@ package com.smartcar.team4.controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.bluetooth.BluetoothSocket;
@@ -21,9 +17,12 @@ import android.os.AsyncTask;
 
 import java.io.IOException;
 import java.util.UUID;
+import com.smartcar.team4.controller.JoystickView.OnJoystickMoveListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private JoystickView joystick;
     TextView test;
     Button btnDis;
     String address = null;
@@ -34,16 +33,13 @@ public class MainActivity extends AppCompatActivity {
     //Not sure if the UUID is correct..
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Recieve the adress from Bluetooth class
         Intent newint = getIntent();
         address = newint.getStringExtra(Bluetooth.EXTRA_ADDRESS);
-
-        setContentView(R.layout.activity_main);
-
-
 
         //Calls the bluetooth connection method.
         new ConnectToBt().execute();
@@ -60,85 +56,121 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        joystick = (JoystickView) findViewById(R.id.game);
+        joystick.setOnJoystickMoveListener(new OnJoystickMoveListener() {
+
+            @Override
+            public void onValueChanged(int angle, int power, int direction) {
+                switch (direction) {
+                    case JoystickView.FRONT:
+                        goForward();
+                        break;
+                    case JoystickView.FRONT_RIGHT:
+                        goLeftFront();
+                        break;
+                    case JoystickView.RIGHT:
+                        goRight();
+                        break;
+                    case JoystickView.RIGHT_BOTTOM:
+                        goLeftBottom();
+                        break;
+                    case JoystickView.BOTTOM:
+                        goReverse();
+                        break;
+                    case JoystickView.BOTTOM_LEFT:
+                        goRightBottom();
+                        break;
+                    case JoystickView.LEFT:
+                        goLeft();
+                        break;
+                    case JoystickView.LEFT_FRONT:
+                        goRightFront();
+                        break;
+                    default: stop();
+
+                }
+            }
+        }, JoystickView.DEFAULT_LOOP_INTERVAL);
+
+
 
         // Button for turning right
-        Button buttonForward = (Button) findViewById(R.id.button_forward);
-        buttonForward.setOnTouchListener(new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch(event.getAction()) {
-                    // when button is pushed down
-                    case MotionEvent.ACTION_DOWN:
-                        goForward();
-                        return true;
-                    // when button is released
-                    case MotionEvent.ACTION_UP:
-                        stop();
-                        return true;
-                }
-                return false;
-            }
-        });
+//        joystick = (JoystickView) findViewById(R.id.game);
+//            joystick.setOnJoystickMoveListener(new OnJoystickMoveListener() {
+//            @Override
+//            public void onValueChanged(int angle, int power, int direction) {
+//                switch(direction) {
+//                    // when button is pushed down
+//                    case JoystickView.FRONT:
+//                        goForward();
+//                        break;
+//                        // when button is released
+//                    default:
+//                        stop();
+//                        break;
+//                }
+//            }
+//        }, JoystickView.DEFAULT_LOOP_INTERVAL);
 
 
         // Button for turning left
-        Button buttonReverse = (Button) findViewById(R.id.button_reverse);
-        buttonReverse.setOnTouchListener(new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch(event.getAction()) {
-                    // when button is pushed down
-                    case MotionEvent.ACTION_DOWN:
-                        goReverse();
-                        return true;
-                    // when button is released
-                    case MotionEvent.ACTION_UP:
-                        stop();
-                        return true;
-                }
-                return false;
-            }
-        });
+//        Button buttonReverse = (Button) findViewById(R.id.button_reverse);
+//        buttonReverse.setOnTouchListener(new OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                switch(event.getAction()) {
+//                    // when button is pushed down
+//                    case MotionEvent.ACTION_DOWN:
+//                        goReverse();
+//                        return true;
+//                    // when button is released
+//                    case MotionEvent.ACTION_UP:
+//                        stop();
+//                        return true;
+//                }
+//                return false;
+//            }
+//        });
 
-        Button buttonLeft = (Button) findViewById(R.id.button_left);
-        buttonLeft.setOnTouchListener(new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch(event.getAction()) {
-                    // when button is pushed down
-                    case MotionEvent.ACTION_DOWN:
-                        goLeft();
-                        return true;
-                    // when button is released
-                    case MotionEvent.ACTION_UP:
-                        stop();
-                        return true;
-                }
-                return false;
-            }
-        });
-
-        Button buttonRight = (Button) findViewById(R.id.button_right);
-        buttonRight.setOnTouchListener(new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch(event.getAction()) {
-                    // when button is pushed down
-                    case MotionEvent.ACTION_DOWN:
-                        goRight();
-                        return true;
-                    // when button is released
-                    case MotionEvent.ACTION_UP:
-                        stop();
-                        return true;
-                }
-                return false;
-            }
-        });
+//        Button buttonLeft = (Button) findViewById(R.id.button_left);
+//        buttonLeft.setOnTouchListener(new OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                switch(event.getAction()) {
+//                    // when button is pushed down
+//                    case MotionEvent.ACTION_DOWN:
+//                        goLeft();
+//                        return true;
+//                    // when button is released
+//                    case MotionEvent.ACTION_UP:
+//                        stop();
+//                        return true;
+//                }
+//                return false;
+//            }
+//        });
+//
+//        Button buttonRight = (Button) findViewById(R.id.button_right);
+//        buttonRight.setOnTouchListener(new OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                switch(event.getAction()) {
+//                    // when button is pushed down
+//                    case MotionEvent.ACTION_DOWN:
+//                        goRight();
+//                        return true;
+//                    // when button is released
+//                    case MotionEvent.ACTION_UP:
+//                        stop();
+//                        return true;
+//                }
+//                return false;
+//            }
+//        });
     }
 
     private void goForward() {
-        if(btSocket != null){
+        if(btSocket != null ){
             try{
                 btSocket.getOutputStream().write("f".toString().getBytes());
             } catch(IOException e){
@@ -171,6 +203,46 @@ public class MainActivity extends AppCompatActivity {
         if(btSocket != null){
             try{
                 btSocket.getOutputStream().write("l".toString().getBytes());
+            } catch(IOException f){
+                f.printStackTrace();
+            }
+        }
+    }
+
+    private void goLeftFront(){
+        if(btSocket != null){
+            try{
+                btSocket.getOutputStream().write("q".toString().getBytes());
+            } catch(IOException f){
+                f.printStackTrace();
+            }
+        }
+    }
+
+    private void goRightFront(){
+        if(btSocket != null){
+            try{
+                btSocket.getOutputStream().write("e".toString().getBytes());
+            } catch(IOException f){
+                f.printStackTrace();
+            }
+        }
+    }
+
+    private void goLeftBottom(){
+        if(btSocket != null){
+            try{
+                btSocket.getOutputStream().write("z".toString().getBytes());
+            } catch(IOException f){
+                f.printStackTrace();
+            }
+        }
+    }
+
+    private void goRightBottom(){
+        if(btSocket != null){
+            try{
+                btSocket.getOutputStream().write("c".toString().getBytes());
             } catch(IOException f){
                 f.printStackTrace();
             }
